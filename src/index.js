b7221d4ececd3d770c10a2c4145c6f195a157bad
keymaster80 <63088504+keymaster80@users.noreply.github.com>
2024-08-30 10:20:15 -0500

    cambios para grupointer
    se hace toda la logica para grupo inter

--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
 const express = require('express');
-const xml2js = require('xml2js');
+const axios = require('axios');
+const { parseStringPromise } = require('xml2js');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
-
 app.use(bodyParser.json());
 
-// Ruta para recibir JSON y convertirlo a XML
-app.post('/convert', (req, res) => {
-    const jsonInput = req.body;
+app.post('/convert', async (req, res) => {
+    const { ApiKey, ValorBusqueda } = req.body;
 
-    // Convertir JSON a XML
-    const builder = new xml2js.Builder();
-    const xmlOutput = builder.buildObject(jsonInput);
+    // Construir el XML Request
+    const xmlRequest = `
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:gxv="GxVisionX_K2BTools">
+            <soapenv:Header/>
+            <soapenv:Body>
+                <gxv:wsGestionAbonado.BUSCARABONADOV2>
+                    <gxv:Entrada>{
+                        "ApiKey":"${ApiKey}",
+                        "ValorBusqueda":"${ValorBusqueda}"
+                    }</gxv:Entrada>
+                </gxv:wsGestionAbonado.BUSCARABONADOV2>
+            </soapenv:Body>
+        </soapenv:Envelope>
+    `;
 
-    res.header('Content-Type', 'application/xml');
-    res.send(xmlOutput);
+    try {
+        // Enviar el request SOAP
+        const { data } = await axios.post('http://200.8.126.162:8080/GxVisionX_K2BToolsJavaEnvironment_GxTest175/servlet/awsgestionabonado?wsdl', xmlRequest, {
+            headers: {
+                'Content-Type': 'text/xml;charset=UTF-8',
+                'SOAPAction': 'BUSCARABONADOV2'
+            }
+        });
+
+        // Convertir la respuesta XML a JSON
+        const parsedResponse = await parseStringPromise(data, { explicitArray: false });
+        const salida = parsedResponse['SOAP-ENV:Envelope']['SOAP-ENV:Body']['wsGestionAbonado.BUSCARABONADOV2Response'].Salida;
+
+        // Enviar la respuesta JSON al cliente
+        res.json(JSON.parse(salida));
+
+    } catch (error) {
+        console.error('Error processing SOAP request:', error);
+        res.status(500).send('Error processing request');
+    }
 });
 
-app.listen(port, () => {
-    console.log(`API corriendo en http://localhost:${port}`);
-    console.log('testing')
+app.listen(3000, () => {
+    console.log('API is running on http://localhost:3000');
 });
-
