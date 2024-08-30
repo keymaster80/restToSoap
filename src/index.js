2bf9e34355e6ec8dab5d0d9b7c5aa923bea7d585
keymaster80 <63088504+keymaster80@users.noreply.github.com>
2024-08-30 11:02:22 -0500

    cambio
    implementacion de he para los caracteres especiales

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const axios = require('axios');
 const { parseStringPromise } = require('xml2js');
 const bodyParser = require('body-parser');
+const he = require('he');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/convert', async (req, res) => {
     const { ApiKey, ValorBusqueda } = req.body;
+
     // Validar los parámetros
     if (!ApiKey || !ValorBusqueda) {
         return res.status(400).send('ApiKey y ValorBusqueda son requeridos');
     }
+
     // Construir el XML Request
     const xmlRequest = `
         <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:gxv="GxVisionX_K2BTools">
@@ -26,8 +29,10 @@ app.post('/convert', async (req, res) => {
             </soapenv:Body>
         </soapenv:Envelope>
     `;
+
     // Log del XML Request
     console.log('XML Request:', xmlRequest);
+
     try {
         // Enviar el request SOAP
         const { data } = await axios.post('http://200.8.126.162:8080/GxVisionX_K2BToolsJavaEnvironment_GxTest175/servlet/awsgestionabonado?wsdl', xmlRequest, {
@@ -36,18 +41,24 @@ app.post('/convert', async (req, res) => {
                 'SOAPAction': 'BUSCARABONADOV2'
             }
         });
-    // Log de la respuesta SOAP
-    console.log('SOAP Response:', data);
+
+        // Log de la respuesta SOAP
+        console.log('SOAP Response:', data);
+
         // Convertir la respuesta XML a JSON
         const parsedResponse = await parseStringPromise(data, { explicitArray: false });
         const salida = parsedResponse['SOAP-ENV:Envelope']['SOAP-ENV:Body']['wsGestionAbonado.BUSCARABONADOV2Response'].Salida;
 
+        // Decodificar entidades HTML y convertir a JSON
+        const decodedSalida = he.decode(salida);
+        const jsonResponse = JSON.parse(decodedSalida);
+
         // Enviar la respuesta JSON al cliente
-        res.json(JSON.parse(salida));
+        res.json(jsonResponse);
 
     } catch (error) {
-        console.error('Error processing SOAP request:', error);
-        res.status(500).send('Error processing request');
+        console.error('Error processing SOAP request:', error.message);
+        res.status(500).send(`Error processing request: ${error.message}`);
     }
 });
 
